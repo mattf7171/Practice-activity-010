5c87b58619f0f386d8ead320495ca4fc26d838be
mattf7171 <mattf7171@gmail.com>
2024-07-20 21:19:38 -0600

    c

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,16 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+let dept = "CS"
+let num = 2350
+let name = 'Jack'
+let greeting = `Welcome`
+
+// 1 Using concatenation
+let msg = greeting + name + "to" + dept + num
+console.log(msg)
+
+//2: using interpolation
+let msg2 = `${greeting} ${name} to 
+${dept}${num}!`
+console.log(msg2)
